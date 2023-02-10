589306e34670944b089a5350e62dddef5ba7bcbb
cmathew <chris.w.mathew@gmail.com>
2023-02-10 15:36:08 -0800

    use AGP 7.2.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-  implementation("com.android.tools.build:gradle:4.0.1")
+  implementation("com.android.tools.build:gradle:7.2.2")
   implementation(kotlin("stdlib-jdk8"))
 }
